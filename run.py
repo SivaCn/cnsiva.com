8240d3c870e86f1133e9ba559961cdf86dbddf46
user name <user@mail.com>
2014-11-11 02:08:48 +0530

    base framework with basic css

---
+++ run.py
@@ -0,0 +1,42 @@
+#coding: utf-8
+
+import bottle
+
+# Static pages
+
+@bottle.route('/')
+def index():
+   """."""
+   return views("frame_template.html")
+
+@bottle.route('/sorry_page')
+def sorry_page():
+    """Serve sorry page"""
+    return '<p>Sorry, you are not authorized to perform this action</p>'
+
+@bottle.get('/<filename:re:.*\.(tpl|html)>')
+def views(filename):
+    return bottle.static_file(filename, root='static/htmls')
+
+@bottle.get('/<filename:re:.*\.js>')
+def javascripts(filename):
+    return bottle.static_file(filename, root='static/jscripts')
+
+@bottle.get('/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return bottle.static_file(filename, root='static/css')
+
+@bottle.get('/<filename:re:.*\.(jpg|png|gif|ico)>')
+def images(filename):
+    return bottle.static_file(filename, root='static/images')
+
+##  Web application main  # #
+def main():
+
+    # Start the Bottle webapp
+    bottle.debug(True)
+    bottle.run(host='0.0.0.0', port=80, reloader=True)
+
+if __name__ == "__main__":
+    main()
+
