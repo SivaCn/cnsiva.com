14a96619bd199e184213c0f0e6f94ec5a64b96f2
Siva Cn <cnsiva.in@gmail.com>
2014-12-08 00:53:12 +0530

    AJAX bug fix

--- static/js/script.js
+++ static/js/script.js
@@ -5,12 +5,15 @@
 * :param: sGetContent: page to be fetched
 */
 function switchMenu(sGetContent) {
+    alert("/get_page_content/"+sGetContent);
     $.ajax({
        url: "/get_page_content/"+sGetContent,
        type: "get",
        data: {},
        success: function(response){
            $('#rightpane').html(response)
+           alert(response);
        }
     });
+    alert('ddd');
 };
