792b7501bc52458467096628b553b1fa2e3f702d
Siva Cn <cnsiva.in@gmail.com>
2015-02-07 13:20:10 +0530

    update dos string

--- WSGI.py
+++ WSGI.py
@@ -2,7 +2,7 @@
 
 """run this file using the following command
 
-<path/to/uwsgi>/uwsgi --enable-threads --http 0.0.0.0:8080 -w WSGI:app
+<path/to/uwsgi>/uwsgi --enable-threads --http 0.0.0.0:8080 -w WSGI:app &
 """
 
 from run import app
