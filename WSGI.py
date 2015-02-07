37691d4765a831ca21f0d8f2dbf4570fea83047a
Siva Cn <cnsiva.in@gmail.com>
2015-02-07 13:18:37 +0530

    correction in doc string

--- WSGI.py
+++ WSGI.py
@@ -2,7 +2,7 @@
 
 """run this file using the following command
 
-uwsgi --enable-threads --http 0.0.0.0:8080 -w WSGI:ap
+<path/to/uwsgi>/uwsgi --enable-threads --http 0.0.0.0:8080 -w WSGI:app
 """
 
 from run import app
