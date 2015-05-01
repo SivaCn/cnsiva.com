75f8f53c61ea1974d1773a5934444185899ad4e0
Siva Cn <cnsiva.in@gmail.com>
2015-05-02 01:33:02 +0530

    updated about_me page

--- cnsiva/quotes.py
+++ cnsiva/quotes.py
@@ -26,20 +26,22 @@ class Quotes(object):
         _template = """
                     <div class="row">
                        <div class="eight columns titled outlined">
-                           <b>cnsiva@OpenSource#</b> python ~/getRandomQuote.py<br>
+                           (venv)<br>
+                           <b>cnsiva@OpenSource#</b> ~/scripts/./getRandomQuote.py<br>
                            %(pre_processor)s
                            <p align="center"><b>%(quote)s</b></p><br>
                            <p align="right">
                                -- <b>%(author)s</b>
                            </p>
+                           (venv)<br>
                            <b>cnsiva@OpenSource#</b> <img src="/cursor_blink_1.gif" height="20" width="10">
                            <br>
                        </div>
-                       <div class="four columns outlined">
+                       <div class="three columns outlined">
                            <br>
                            %(user_info)s
                            <br>
-                           <font size="-2" color="red"> *** NOTE: None of your informations are being stored</font>
+                           <font size="-2" color="red"> <b>*** NOTE: No data is stored</b></font>
                        </div>
                     </div>"""
 
@@ -48,7 +50,7 @@ class Quotes(object):
                    'author': _author,
                    'user_info': None}
 
-        _user_template = """<b>Your IP Address: </b>%(ip)s"""
+        _user_template = """<b>Your IP is </b>%(ip)s"""
 
         _user_info = {'ip': self.context.request.remote_addr or 'Unknown'}
 
